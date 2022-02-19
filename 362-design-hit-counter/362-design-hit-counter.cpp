a5d5662438d003525b9dee3375f203929fef840b
Mohd Zaid <diaz.zidane@gmail.com>
2022-02-18 17:42:07 -0700

    Time: 0 ms (100.00%), Space: 7.2 MB (83.20%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.cpp
+++ 362-design-hit-counter/362-design-hit-counter.cpp
@@ -1,38 +1,28 @@
 class HitCounter {
+private:
+    
+    queue<int> hits;
+    
 public:
     
-    
-    vector<int>ans;
+    /** Initialize your data structure here. */
     HitCounter() {
-             
-        
     }
     
-    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
     void hit(int timestamp) {
-        
-         ans.push_back(timestamp);
-        
-        
+        this->hits.push(timestamp);
     }
     
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
     int getHits(int timestamp) {
-        
-        int cnt=0;
-        for(int i=0;i<ans.size();i++){
-                
-                if(ans[i]>timestamp-300){
-                        cnt++;
-                    }
+        while (!this->hits.empty()) {
+            int diff = timestamp - this->hits.front();
+            if (diff >= 300) this->hits.pop();
+            else break;
         }
-        return cnt;
-        
+        return this->hits.size();
     }
 };
-
-/**
- * Your HitCounter object will be instantiated and called as such:
- * HitCounter* obj = new HitCounter();
- * obj->hit(timestamp);
- * int param_2 = obj->getHits(timestamp);
- */
