2b2fde054cdf524f744f030cc1d022aff9b20e0a
Mohd Zaid <diaz.zidane@gmail.com>
2022-03-06 16:42:23 -0700

    Time: 97 ms (6.37%), Space: 30.5 MB (5.10%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+   int MOD=1e9+7;
+    
+    int countOrders(int n) {
+        
+        vector<vector<long>>dp(n+1,vector<long>(n+1,0));
+        
+        for(int unpicked=0;unpicked<=n;unpicked++){
+                for(int undelivered=unpicked;undelivered<=n;undelivered++){
+                    
+                        if(unpicked==0 and undelivered==0){
+                            
+                                dp[unpicked][undelivered]=1;                
+                                continue;
+                        }
+
+                if(unpicked>0){
+                        
+                            dp[unpicked][undelivered]+=unpicked*dp[unpicked-1][undelivered];        
+                            
+                }
+            
+                dp[unpicked][undelivered] %= MOD;
+            
+                
+                if(undelivered>unpicked)
+                {
+                    
+                dp[unpicked][undelivered]+=(undelivered-unpicked)*dp[unpicked][undelivered-1];
+                    
+                }
+            
+                dp[unpicked][undelivered]%=MOD;
+            }
+            
+        }
+        
+        return dp[n][n];
+    }
+};
