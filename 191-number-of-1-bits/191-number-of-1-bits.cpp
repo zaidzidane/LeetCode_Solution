4ec42273c873d2fb93c69a53831d0561f3250b80
Mohd Zaid <diaz.zidane@gmail.com>
2022-03-09 12:11:25 -0700

    Time: 0 ms (100.00%), Space: 6 MB (48.28%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,10 +4,13 @@ public:
         
             int cnt=0;
             while(n!=0){
-                    n&1==1?cnt++:0;
-                    n=n>>1;
-                
+                    
+                    n=n&n-1;
+                    cnt++;
+                    
             }
+            
             return cnt;
+        
     }
 };
