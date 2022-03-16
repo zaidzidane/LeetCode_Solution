3e561f92488e37a235310a7f6b5d16391593ef69
Mohd Zaid <diaz.zidane@gmail.com>
2022-03-16 01:51:52 -0700

    Time: 6 ms (8.58%), Space: 6.1 MB (82.64%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        
+            for(int i=0;i<s.length();i++){
+                
+                    if(s[i]>=65 and s[i]<=90){
+                            
+                            s[i]+=32;
+                    }
+                    
+                
+            }
+        
+       return s;
+    }
+};
