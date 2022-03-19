61e0068ee42e0f8eabdaca03b7a79ffbb82c3478
Mohd Zaid <diaz.zidane@gmail.com>
2022-03-18 21:42:05 -0700

    Time: 139 ms (5.31%), Space: 33.2 MB (35.63%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,57 +1,16 @@
 class ParkingSystem {
 public:
     
-    
-    int cnt1=0;
-    int cnt2=0;
-    int cnt3=0;
+    vector<int>count;
     ParkingSystem(int big, int medium, int small) {
-                cnt1=big;
-                cnt2=medium;
-                cnt3=small;
+        
+            count={big,medium,small};
+        
     }
     
     bool addCar(int carType) {
         
-                if(carType==1){
-                        
-                        if(cnt1>0){
-                                cnt1--;
-                                return true;
-                        }
-                        else{
-                                return false;
-                        }
-                        
-                }  
-        
-        
-                if(carType==2){
-                        
-                        if(cnt2>0){
-                                cnt2--;
-                                return true;
-                        }
-                        else{
-                                return false;
-                        }
-                        
-                }
-        
-            
-                if(carType==3){
-                        
-                        if(cnt3>0){
-                                cnt3--;
-                                return true;
-                        }
-                        else{
-                                return false;
-                        }
-                        
-                }
-                
-                return false;
+            return count[carType-1]-->0;
     }
 };
 
