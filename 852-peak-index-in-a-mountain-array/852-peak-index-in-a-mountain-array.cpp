e911e7441d05632c74d3b6e4d92ea7888d00b14b
Mohd Zaid <diaz.zidane@gmail.com>
2022-08-08 17:10:42 -0700

    Time: 120 ms (44.62%), Space: 59.6 MB (43.89%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+                int left=0;
+                int right=arr.size()-1;
+                while(left<right){
+                        int mid =left+(right-left)/2;
+                        cout<<left<<"\t"<<right<<endl;
+                    
+                        if(arr[mid]>arr[mid+1]){
+                                
+                                        right=mid;        
+                        }
+                        else{
+                                        left=mid+1;   
+                        }
+                }
+        
+                return right;
+                
+    }
+};
