338e770e4d7d1897c720bb53ea40b2cfe79a697b
Mohd Zaid <diaz.zidane@gmail.com>
2022-08-12 02:43:42 -0700

    Time: 245 ms (57.86%), Space: 107 MB (5.25%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -2,60 +2,43 @@ class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
         
-        bool val=true;
-        int flag=1;
-        for(int i=1;i<nums.size();i++){
-                    if(nums[i]-nums[i-1]==0 and flag){
-                             continue;      
-                    }  
-                    else if(flag==1){
-                            val=nums[i]-nums[i-1]>0?true:false;
-                            flag=0;
-                    
-                    }
-            
-                    else{
-                        
-                            if(val){
-                                
-                                    if(nums[i]-nums[i-1]>=0){
-                                            
-                                                continue;
-                                    }
-                                    else{
-                                            
-                                                return  false;
-                                        
-                                            
-                                    }
-                                
-                                
-                            }
-                        
-                        
-                            if(!val){
-                                
-                                    if(nums[i]-nums[i-1]<=0){
-                                            
-                                                continue;
-                                    }
-                                    else{
-                                            
-                                                return  false;
-                                        
-                                            
-                                    }
-                                
-                                
-                            }
-                           
-                        
-                    }
-            
-        }
         
+            vector<int>ans;
+            for(int i=1;i<nums.size();i++){
+                        ans.push_back(nums[i]-nums[i-1]);
+            }
+            
+            int flag=-1;
+            for(int i=0;i<ans.size();i++){
+                
+                       
+                        if(ans[i]==0){
+                                continue;
+                        }
+                
+                        if(ans[i]>0 and (flag==-1 or flag==1) ){
+                                    flag=1;
+                                    continue;
+                            
+                        }
+                         if(ans[i]<0 and (flag==-1 or flag==0) ){
+                                    flag=0;
+                                    continue;
+                        }
+                
+                        else{
+                                
+                                    return false;
+                            
+                            
+                        }
+                
+                
+                
+            }
         
             return true;
+        
         
     }
 };
