9a61dabd09e52d1b8cdce53fcbe50873c69a60c8
Mohd Zaid <diaz.zidane@gmail.com>
2022-08-20 02:20:25 -0700

    Time: 3 ms (59.97%), Space: 5.9 MB (70.35%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+            if(n<=0){
+                    return false;
+            }  
+        
+            return((n&n-1)==0);
+        
+        
+    }
+};
