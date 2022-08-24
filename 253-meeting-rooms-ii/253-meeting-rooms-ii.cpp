1aaa7340a614623e053e799cf2b4ff1ad895913f
Mohd Zaid <diaz.zidane@gmail.com>
2022-08-24 13:03:08 -0700

    Time: 36 ms (32.56%), Space: 11.8 MB (69.05%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        
+        
+            sort(intervals.begin(),intervals.end());
+        
+            priority_queue<int,vector<int>,greater<int>>pq;
+            for(int i=0;i<intervals.size();i++){
+                    
+                    if(pq.empty())
+                        pq.push(intervals[i][1]);    
+                    else{
+                        
+                        if(pq.top()<=intervals[i][0])
+                        {
+                                pq.pop();
+                                
+                            
+                        }
+                        
+                        
+                
+                        pq.push(intervals[i][1]); 
+                    }
+                        
+                
+            }
+            
+            return pq.size();
+    }
+};
