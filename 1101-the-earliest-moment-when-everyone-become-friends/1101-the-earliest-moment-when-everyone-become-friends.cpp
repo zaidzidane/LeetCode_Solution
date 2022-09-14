fcc6da9f31aeaef6c5bfa5e883dcf8c06fe4dbb1
Mohd Zaid <diaz.zidane@gmail.com>
2022-09-14 14:17:19 -0700

    Time: 52 ms (43.87%), Space: 18.8 MB (9.49%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
@@ -1,75 +1,83 @@
 class Solution {
 public:
-        class DSU{
+    
+    int find(vector<int>&root,int x){
         
-        public:
-        vector<int>root;
-        vector<int>rank;
-        int count;
-        
-        DSU(int n){
-                count=n;
-                for(int i=0;i<n;i++){
-                        root.push_back(i);
-                        rank.push_back(1);
-                }    
-        }
-        int findset(int x){
-                if(x==root[x]){
+                while(x==root[x]){
+                    
                         return x;
                 }
-                return root[x]=findset(root[x]);
-        }
-        
-        void unionset(int x,int y){
-                int rootx=findset(x);
-                int rooty=findset(y);
-                if(rootx!=rooty){
-                            
-                        if(rank[rootx]>rank[rooty]){
-                                root[rooty]=rootx;
-                        }
-                        else if(rank[rooty]>rank[rootx]){
-                                root[rootx]=rooty;
-                        }
-                        else{
-                                root[rooty]=rootx;
-                                rank[rootx] += 1;
-                        }
-                    count--;
-                }       
-        }
-           
-    };
+                    
+                return root[x]=find(root,root[x]);
+    }   
     
-    static bool compare(vector<int>a,vector<int>b){
+    
+    
+    void unionfind(vector<int>&root,vector<int>&rank,int i,int j){
         
-            return a[0]<b[0];
+        
+                    int rootx=find(root,i);
+                    int rooty=find(root,j);
+                    if(rootx!=rooty){
+                        
+                                if(rank[rootx]>rank[rooty]){
+                                    
+                                        rank[rootx]+=rank[rooty];
+                                        root[rooty]=rootx;
+                                        for(int i=0;i<root.size();i++){
+                                            
+                                                    if(root[i]==rooty) root[i]=rootx;
+                                            
+                                        }
+                                    
+                                }
+                            
+                                else{
+                                    
+                                        rank[rooty]+=rank[rootx];
+                                        root[rootx]=rooty;
+                                         for(int i=0;i<root.size();i++){
+                                            
+                                                    if(root[i]==rootx) root[i]=rooty;
+                                            
+                                        }
+                                    
+                                }
+                        
+                    }
+            
+                    
+                    
+        
+        
+        
     }
     
+    
     int earliestAcq(vector<vector<int>>& logs, int n) {
+     
+                    sort(logs.begin(),logs.end());
+                    vector<int>root(n,-1);
+                    vector<int>rank(n,1);
         
-        DSU g(n); 
-        sort(logs.begin(),logs.end(),compare);
-        int timestamp;
-        for(vector<int>edge:logs){
-                    int first=edge[0];
-                    int second=edge[1];
-                    int third=edge[2];
-                    cout<<first<<second<<third<<endl;
-                    int s1=g.findset(second);
-                    int s2=g.findset(third);
-                    if(s1!=s2){
-                                timestamp=first;
-                                g.unionset(s1,s2);
+                    for(int i=0;i<root.size();i++){
+                        
+                                root[i]=i;
+                        
                     }
-        }
         
-        if(g.count>1){
-            return -1;
-        }
+                   for(auto log:logs){
+                       
+                                unionfind(root,rank,log[1],log[2]);
+                                set<int>s(root.begin(),root.end());
+                                if(s.size()==1){
+                                        return log[0];
+                                }
+                       
+                   }
         
+                    return -1;
+                
         
-       return timestamp; 
     }
 };
