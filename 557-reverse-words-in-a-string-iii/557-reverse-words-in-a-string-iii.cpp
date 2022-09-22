b51b6d6cb83dc25b76e6471485a4d5d3f796899e
Mohd Zaid <diaz.zidane@gmail.com>
2022-09-21 23:53:31 -0700

    Time: 57 ms (13.04%), Space: 14.5 MB (13.61%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
-        
-        stringstream s1(s);
-        string word;
-        int count=0;
-        string new_str="";
-        while(s1>>word){
-                reverse(word.begin(),word.end());
-                new_str+= word+" ";
+     
+        string ans="";
+        string word="";
+        stringstream ss(s);
+        while(ss>>word){
+            
+            reverse(word.begin(),word.end());
+            ans+=word+' ';
+            
         }
         
-        return new_str.substr(0,new_str.size()-1);
-        
+        return ans.substr(0,ans.size()-1);
     }
 };
