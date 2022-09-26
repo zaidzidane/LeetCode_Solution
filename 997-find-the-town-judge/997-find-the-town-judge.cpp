d1c3fef6a63b52eea1a82ddaaaf43746bc72b9d3
Mohd Zaid <diaz.zidane@gmail.com>
2022-09-26 12:58:42 -0700

    Time: 427 ms (16.36%), Space: 60.8 MB (68.52%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+     
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        vector<int>indegree(n+1,0);
+        vector<int>outdegree(n+1,0);
+        
+        for(int i=0;i<trust.size();i++){
+                    indegree[trust[i][1]]++;
+                    outdegree[trust[i][0]]++;  
+        }
+        
+        
+        for(int i=1;i<=n;i++){
+            
+               cout<<indegree[i]<<"\t"<<outdegree[i]<<endl;
+            
+        }
+        
+        
+        
+        for(int i=1;i<=n;i++){
+            
+                if(indegree[i]==n-1 and outdegree[i]==0){
+                    
+                            return i;
+                    
+                }
+            
+        }
+        
+        return -1;
+        
+    }
+};
