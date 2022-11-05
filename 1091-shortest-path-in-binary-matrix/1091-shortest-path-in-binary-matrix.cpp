cf1b71fe169a8e7844e16ba92889ba8f76cd5d4b
Mohd Zaid <diaz.zidane@gmail.com>
2022-11-05 15:49:37 -0700

    Time: 392 ms (15.90%), Space: 32.1 MB (25.21%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+     
+            vector<vector<int>>directions{{0,1},{0,-1},{1,0},{1,1},{-1,1},{-1,-1},{-1,0},{1,-1}};
         
-            int m=grid.size()-1;
-            int n=grid[0].size()-1;
-            
-            if(grid[0][0] or grid[m][n]) return -1;
-            if(m==0 or n==0) return 1;
-            vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(),false));
             queue<pair<int,int>>q;
-            vector<vector<int>>check{{-1,0},{1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
-            vector<vector<int>>distance(m+1,vector<int>(n+1,0));
-            q.push({0,0});
-            int len=1;
-            distance[0][0]=1;
-            
-            while(!q.empty()){
-                
-                    auto temp=q.front();
-                    q.pop();
-                    for(int i=0;i<check.size();i++){
-                        
-                            int x=temp.first+check[i][0];
-                            int y=temp.second+check[i][1];
-                            
-                            if(x>=0 and x<=m and y>=0 and y<=n and visited[x][y]==false and grid[x][y]!=1) {
-                                
-                                        q.push({x,y});
-                                        visited[x][y]=true;
-                                        distance[x][y]=distance[temp.first][temp.second]+1;
-                                        if(x==m and y==n){
-                                            
-                                                return  distance[x][y];
-                                        }
-                                
-                            }
-                        
-                        
-                    }
-                
+            if(grid[0][0]==1 or grid[grid.size()-1][grid[0].size()-1]==1){
+                    return -1;
             }
         
+            q.push({0,0});
+            int cnt=0;
+            map<pair<int,int>,int>gzip;
+            gzip[{0,0}]=1;
+            while(!q.empty()){
+                
+                    pair<int,int>temp=q.front();
+                    q.pop();
+                    
+                    for(int i=0;i<directions.size();i++){
         
+                            int x=temp.first+directions[i][0];
+                            int y=temp.second+directions[i][1];
+                        
+                            if(temp.first==grid.size()-1 and temp.second==grid[0].size()-1){
+                                
+                                        return gzip[{temp.first,temp.second}];
+                                    
+                            }
+                            if(x>=0 and x<grid.size() and y>=0 and y<grid.size() and grid[x][y]==0 and gzip.find({x,y})==gzip.end()){
+                                    
+                                            gzip[{x,y}]=gzip[{temp.first,temp.second}]+1;
+                                            q.push({x,y});
+                                
+                                
+                            }
+                    
+                    }
+                
+                
+                
+            }
+     
             return -1;
-        
     }
+    
 };
