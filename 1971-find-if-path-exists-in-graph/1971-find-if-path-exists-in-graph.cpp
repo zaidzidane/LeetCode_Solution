a3731fff6fbcbc7f99a8a927d2778460a8a6a3f6
Mohd Zaid <diaz.zidane@gmail.com>
2022-11-05 12:35:53 -0700

    Time: 1245 ms (58.46%), Space: 149.3 MB (77.10%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,57 +1,41 @@
-    class Solution {
+class Solution {
 public:
-    
-        
-    bool dfs(int source,int destination,vector<bool>&visited,vector<vector<int>>&nbrs){
-    
-        
-                if(visited[source]==true){
-                    
-                        return false;    
-                    
-                }
-                visited[source]=true;
-                // cout<<source<<endl;
-                if(source==destination){
-                        return true;
-                }
-                // cout<<nbrs[source].size()<<endl;
-                for(int i=0;i<nbrs[source].size();i++){
-                    
-                              if(dfs(nbrs[source][i],destination,visited,nbrs)){
-                                        return true;
-                              }
-                            else{
-                                
-                                        continue;    
-                                
-                            }
-                        
-                }
-        
-        
-                return false;
-        
-    }
-        
-    
-    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         
-            vector<bool>visited(n,false);
-            vector<vector<int>>nbrs(n);    
-        
-            for(int i=0;i<edges.size();i++){
-                
-                           nbrs[edges[i][0]].push_back(edges[i][1]); 
-                            nbrs[edges[i][1]].push_back(edges[i][0]); 
+            
+                vector<vector<int>>links(n);
+                for(int i=0;i<edges.size();i++){
                     
-            }
-            
-            return dfs(source,destination,visited,nbrs);
-            
+                            links[edges[i][0]].push_back(edges[i][1]);
+                            links[edges[i][1]].push_back(edges[i][0]);
+                    
+                }
         
-        
-        
+                vector<bool>visited(n);
+                queue<int>s;
+                s.push(source);
+                visited[source]=true;
+                while(!s.empty()){
+                    
+                            int temp=s.front();    
+                            cout<<temp<<endl;
+                            s.pop();
+                            if(temp==destination){
+                                    return true;
+                            }        
+                            for(auto nbrs:links[temp]){
+                                
+                                        if(!visited[nbrs]){
+                                            s.push(nbrs);
+                                            visited[nbrs]=true;
+                                        }
+                            }
+                            
+                            
+                    
+                    
+                }
+                
+            return false;
     }
 };
