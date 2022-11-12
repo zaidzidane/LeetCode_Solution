807478705eb05129827a7e3a3fdd570202c40b87
Mohd Zaid <diaz.zidane@gmail.com>
2022-11-12 16:56:35 -0700

    Time: 7 ms (98.92%), Space: 10.8 MB (50.79%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+            unordered_map<int,int>gzip;
+            
+            for(int i=0;i<nums.size();i++){
+                
+                    if(gzip.find(target-nums[i])!=gzip.end()){
+                            
+                            return {gzip[target-nums[i]],i};
+                                
+                    }
+                    else{
+                        
+                            gzip[nums[i]]=i;
+                        
+                    }
+                    
+                
+                
+            }
+        
+            return {};
+        
+    }
+};
