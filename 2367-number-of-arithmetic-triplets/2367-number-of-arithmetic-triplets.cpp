7efc9a76cfceb522b7f664b33ad3e752cef3c212
Mohd Zaid <diaz.zidane@gmail.com>
2022-11-12 15:20:39 -0700

    Time: 4 ms (88.72%), Space: 9.1 MB (15.89%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-  
-            unordered_map<int,int>gzip;
         
+            unordered_map<int,bool>gzip;
+            
             for(int i=0;i<nums.size();i++){
-                    gzip[nums[i]*2]=i;
+                
+                    gzip[nums[i]]=true;
+                
+            }
+                
+            int cnt=0;
+            for(int i=0;i<nums.size();i++){
+                
+                    if(gzip.find(nums[i]+diff)!=gzip.end() and gzip.find(nums[i]-diff)!=gzip.end()){
+                                    cnt+=1;             
+                    }
             }
         
-        
-            int cnt=0;
-            
-           for(int i=0;i<nums.size();i++){
-               for(int j=i+1;j<nums.size();j++){
-                            int idx=gzip[(nums[i]+nums[j])];
-                            if(gzip.find((nums[i]+nums[j]))!=gzip.end()){
-                                            if(i<gzip[(nums[i]+nums[j])] and gzip[(nums[i]+nums[j])]<j and 
-                                                  nums[idx]-nums[i]==diff and nums[j]-nums[idx]==diff  ){
-                                                    cout<<i<<gzip[(nums[i]+nums[j])]<<j<<endl;
-                                                    cnt++;
-                                            }        
-                                
-                            }
-               
-               
-               
-               }
-               
-               
-               
-           }
-            
             return cnt;
-        
         
     }
 };
