980adf89409aad7b36e5925df14b94182e9987f4
Mohd Zaid <diaz.zidane@gmail.com>
2023-03-09 21:37:05 -0700

    Time: 41 ms (56.65%), Space: 13.9 MB (45.50%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+            left=0
+            right=x
+            
+            while(left<=right):
+                
+                mid=left+(right-left)//2
+                if(mid*mid==x):
+                        return mid
+                
+                elif(mid*mid<x):
+                        
+                        left=mid+1
+                        
+                else:
+                        right=mid-1
+                        
+        
+            return right
+            
+        
