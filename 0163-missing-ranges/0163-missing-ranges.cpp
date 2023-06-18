af095322176d8ccb5cf9ef4e6ae882f48f8dca6a
Mohd Zaid <diaz.zidane@gmail.com>
2023-06-18 01:09:37 -0600

    Time: 0 ms (100.00%), Space: 6.9 MB (72.48%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.cpp
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -1,77 +1,44 @@
 class Solution {
 public:
-    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
         
-            unordered_map<int,int>gzip;
-            vector<string>ans;
-            int i=0;
-            int n=nums.size()-1;
-            if(nums.size()==0){
-                
-                 if(upper>=lower){
-                
-                    if(upper-lower==0){
-                                
-                                ans.push_back(to_string(upper));
-                        }
-                
-                        else 
-                                
-                                ans.push_back(to_string(lower)+"->"+to_string(upper));
-                               
+             vector<vector<int>>ans;
+            if(!nums.size()){
+                      
+                    return {{lower,upper}};
             }
+            
+            
                 
-                
-                return ans;  
+            if(nums[0]-lower>=1){
+
+                    ans.push_back({lower,nums[0]-1});
             }
-        
-            if(nums[0]>lower){
+                   
                 
-                    if(nums[0]-lower==1){
-                                
-                                ans.push_back(to_string(lower));
-                        }
-                
-                        else if(nums[0]-lower>1){
-                                
-                                ans.push_back(to_string(lower)+"->"+to_string(nums[0]-1));
-                        }       
-            }
-        
-            while(i<nums.size()-1){
-                
-                        if(nums[i+1]-nums[i]==2){
-                                
-                                ans.push_back(to_string(nums[i]+1));
-                        }
-                
-                        else if(nums[i+1]-nums[i]>2){
-                                
-                                ans.push_back(to_string(nums[i]+1)+"->"+to_string(nums[i+1]-1));
-                        }  
-                
-                        i++;
-                    
-                
-            }
-        
-            if(nums[n]<upper){
-                
-                    if(upper-nums[n]==1){
-                                
-                                ans.push_back(to_string(nums[n]+1));
-                        }
-                
-                        else if(upper-nums[n]>1){
-                                
-                                ans.push_back(to_string(nums[n]+1)+"->"+to_string(upper));
-                        }  
-                    
-                
-            }
-        
             
         
+        
+           
+            for(int i=1;i<nums.size();i++){
+                
+                    if(nums[i]-nums[i-1]==1){
+                            continue;
+                    }
+                    else{
+                        
+                            ans.push_back({nums[i-1]+1,nums[i]-1});
+                        
+                    }
+                
+            }
+        
+            if(upper-nums[nums.size()-1]>=1){
+                        
+                        ans.push_back({nums[nums.size()-1]+1,upper});
+            }
+        
             return ans;
+        
     }
 };
