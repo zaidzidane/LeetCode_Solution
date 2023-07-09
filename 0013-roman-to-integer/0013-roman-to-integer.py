17b95051d1e735993b046a20f64e488924ce62fa
Mohd Zaid <diaz.zidane@gmail.com>
2023-07-09 12:34:13 -0600

    Time: 62 ms (67.28%), Space: 16.2 MB (98.85%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,24 +1,27 @@
 class Solution:
-    
     def romanToInt(self, s: str) -> int:
         
-        d={}
-        d['I']=1
-        d['V']=5
-        d['X']=10
-        d['L']=50
-        d['C']=100
-        d['D']=500
-        d['M']=1000
+            d={}
+            d['I']=1
+            d['V']=5
+            d['X']=10
+            d['L']=50
+            d['C']=100
+            d['D']=500
+            d['M']=1000
         
-        ans=0
-        for i in range(len(s)):
-            
-            if(i>=1 and d[s[i-1]]<d[s[i]]):
-                
-                        ans+=d[s[i]]-2*d[s[i-1]]
+            sum=0
+            for i in reversed(range(len(s))):
                 
                 
-            else:
-                        ans+=d[s[i]]
-        return ans
+                if i<len(s)-1 and d[s[i]]<d[s[i+1]]:
+                        sum-=d[s[i]]
+                        
+                else:
+                        sum+=d[s[i]]
+                 
+               
+             
+            return sum
+                
+                
