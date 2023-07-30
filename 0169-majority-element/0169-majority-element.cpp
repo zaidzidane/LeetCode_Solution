30ee1ca9023e5809e3ed9a3690efb9f98a35fa84
Mohd Zaid <diaz.zidane@gmail.com>
2023-07-29 21:41:42 -0600

    Time: 11 ms (98.41%), Space: 19.6 MB (12.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+            
+        int n=nums.size();
+        int majority_element =0;
+        int bit=1;
         
-            int cnt=1;
-            int temp = nums[0];
+        for(int i=0;i<32;i++){
+            
+                // int bit_count = sum(bool(nums[i] & bit) for num in nums)
+                bit = 1<<i; 
+                int bit_count = 0;
+                for(auto num : nums){
+                        if(num& bit)
+                                bit_count+=1;
+                }
+                //cout<<bit_count<<endl;
+                if(bit_count > n/2){
+                        majority_element+= bit;
+                }    
+                             
+        }
         
-            for(int i=1;i<nums.size();i++){
-                
-                    if(temp!=nums[i]){
-                        
-                            cnt--;
-                            if(cnt==0){
-                                    
-                                    temp=nums[i];
-                                    cnt=1;
-                            }
-                        
-                    }
-                    else{
-                        
-                                cnt++;
-                                
-                        
-                    }
-                
-                
-            }
+        int cnt=0;
+        for(auto num:nums){
+            
+                if(num<0){
+                    
+                        cnt+=1;
+                }
+        }
         
-            return temp;
+        int is_negative=0;
+        if(cnt>n/2){
+                is_negative=1;
+        }
+        
+//         if (is_negative){
+            
+//                     majority_element-=bit;
+//         }
+            
+        return majority_element;
+        
         
     }
 };
