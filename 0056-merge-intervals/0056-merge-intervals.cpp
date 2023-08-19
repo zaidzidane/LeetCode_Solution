b3530c3a594584797df26511032caa6fe010360f
Mohd Zaid <diaz.zidane@gmail.com>
2023-08-19 16:40:08 -0600

    Time: 153 ms (8.10%), Space: 48.2 MB (5.00%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,49 +1,44 @@
+bool compare(vector<int>v1,vector<int>v2){
+            return v1[0]<v2[0]?true:(v1[0]==v2[0]?(v1[1]<v2[1]?true:false):false);
+}
+
+
+
 class Solution {
 public:
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-            sort(intervals.begin(),intervals.end());
+        sort(intervals.begin(),intervals.end(),compare);
         
-            vector<vector<int>>ans;
+        
+//         for(int i=0;i<intervals.size();i++){
             
-            for(int i=0;i<intervals.size();i++){
-                
-                        if(ans.size()==0){
-                            
+//                 cout<<intervals[i][0]<<"\t"<<intervals[i][1]<<endl;
+            
+            
+//         }
+        
+        vector<vector<int>>ans;
+        
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+                    if(intervals[i][0]<=ans.back()[1]){
+                                if(intervals[i][1]<=ans.back()[1]){
+                                        continue;
+                                }
+                                int temp=ans.back()[0];
+                                ans.pop_back();
+                                ans.push_back({temp,intervals[i][1]});    
+                    }
+            
+                    else{
                                 ans.push_back(intervals[i]);
-                            
-                        }
-                
-                        else{
-                            
-                                    
-                                if(ans.back()[1]>=intervals[i][1]){
-                                    
-                                            continue;
-                                    
-                                }
-                            
-                                else if(ans.back()[1]<intervals[i][0]){
-                                        
-                                            ans.push_back(intervals[i]);
-                                }
-                            
-                                else if(ans.back()[1]>=intervals[i][0] and ans.back()[1]<intervals[i][1]){
-                                    
-                                            int x=ans.back()[0];
-                                            int y=intervals[i][1];
-                                            ans.pop_back();
-                                            ans.push_back({x,y});
-                                    
-                                }
-                            
-                        }
-                
-            }
-                        return ans;
-                
+                    }
             
+        }
         
+        return ans;
         
         
     }
