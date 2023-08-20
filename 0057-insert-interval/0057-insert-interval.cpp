027e422b56ea540cb23b471ee3eeffdeff69baa7
Mohd Zaid <diaz.zidane@gmail.com>
2023-08-19 19:59:33 -0600

    Time: 17 ms (55.76%), Space: 18.2 MB (5.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,102 +1,85 @@
+bool compare(vector<int>v1,vector<int>v2){
+            return v1[0]<v2[0]?true:(v1[0]==v2[0]?(v1[1]<v2[1]?true:false):false);
+}
+
+
+
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
+            int i=0;
+            vector<vector<int>>ans;
+            //ans.push_back(intervals[0]);
+            if(intervals.size()==0){
+                    return {newInterval};
+            }
+            bool flag=true;
         
-        if(!intervals.size()){
-                return {newInterval};
-        }
+            while(i<intervals.size()){
+                       
+                    if(ans.size()!=0 and intervals[i][0]<=ans.back()[1]){
+                            if(intervals[i][1]<ans.back()[1]){
+                                    i++;
+                                    continue;
+                            }
+                            int temp=ans.back()[0];
+                            ans.pop_back();
+                            ans.push_back({temp,intervals[i][1]});          
+                    }
+                    else{
+                                ans.push_back(intervals[i]);           
+                    }
+                    i++;
+                        if(ans.back()[1]<newInterval[0] or ans.back()[0]>newInterval[1]){
+                                
+                                continue;
+                        }
+
+                        else if(ans.back()[1]>=newInterval[0] and ans.back()[1]<=newInterval[1]){
+
+                                    flag=false;
+                                    cout<<"here";
+                                    if(newInterval[0]<=ans.back()[0]){
+
+                                                ans.pop_back();
+                                                ans.push_back(newInterval);
+
+                                    }
+                                    else{
+
+                                            int temp = ans.back()[0];
+                                            cout<<ans.size()<<endl;
+                                            ans.pop_back();
+                                            
+                                            ans.push_back({temp,newInterval[1]});
+                                    } 
+
+                        }
+
+                        else if(ans.back()[1]>=newInterval[1] and ans.back()[0]>=newInterval[0]){
+
+                                    flag=false;
+                                   cout<<"here";
+                                   int temp=ans.back()[1];
+                                   ans.pop_back();
+                                   ans.push_back({newInterval[0],temp});
+
+                        }  
+                        else{
+                            
+                            flag=false;
+                            
+                        }
+            }
         
-        int i=0;
-        vector<vector<int>>ans;
-        while(i<intervals.size()){
-                
-                if(intervals[i][1]<newInterval[0]){
+            
+            if(flag){
                     
-                    
-                            ans.push_back(intervals[i]);            
-                    
-                    
-                    
-                }
-            
-                else{
-                    
-                        
-                        break;
-                }
-            
-                i++;
-            
-        }
+                    ans.push_back(newInterval);
+            }
         
-        
-        
-        if(intervals.size()==ans.size()){
-            
-                ans.push_back(newInterval);
-                return ans;
-            
-            
-        }
-        
-        
-        if(intervals[i][1]>=newInterval[1] and intervals[i][0]<=newInterval[0]){
-                
-                 ans.push_back(intervals[i]);  
-        }
-        else if(intervals[i][0]>newInterval[0] and intervals[i][0]>newInterval[1]){
-            
-                 ans.push_back(newInterval); 
-                 i-=1;
-            
-        }
-        else if(intervals[i][0]>newInterval[0] and intervals[i][1]>=newInterval[1]){
-            
-                ans.push_back({newInterval[0],intervals[i][1]}); 
-        }
-        else if(intervals[i][0]<=newInterval[0] and intervals[i][1]<newInterval[1]){
-            
-                ans.push_back({intervals[i][0],newInterval[1]});   
-        }
-        else{
-            
-                ans.push_back(newInterval); 
-        }
-       
-        
-        
-       i++;
-        
-       while(i<intervals.size()){
-   
-           
-           if(ans.back()[1]>=intervals[i][1]){
-               
-                    i+=1;
-                    continue;    
-           }
-           
-           else if(ans.back()[1]<intervals[i][1] and ans.back()[1]>=intervals[i][0]){
-               
-                    int val=ans.back()[0];
-                    ans.pop_back();
-                    ans.push_back({val,intervals[i][1]});
-               
-           }
-           
-           else{
-               
-                ans.push_back(intervals[i]);
-               
-           }
-           
-           
-            i+=1; 
-       }
-        
-        
-        return ans;
-        
+           sort(ans.begin(),ans.end(),compare);
+            return ans; 
     }
 };
