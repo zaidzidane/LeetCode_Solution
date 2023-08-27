a6d7fdba4a8e21487e7901124e6fc84807d496e4
Mohd Zaid <diaz.zidane@gmail.com>
2023-08-27 11:07:16 -0600

    Time: 124 ms (21.52%), Space: 38.8 MB (12.20%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,56 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-     
-            vector<vector<int>>directions{{0,1},{0,-1},{1,0},{1,1},{-1,1},{-1,-1},{-1,0},{1,-1}};
-        
-            queue<pair<int,int>>q;
-            if(grid[0][0]==1 or grid[grid.size()-1][grid[0].size()-1]==1){
-                    return -1;
-            }
-        
-            q.push({0,0});
-            int cnt=0;
-            map<pair<int,int>,int>gzip;
-            gzip[{0,0}]=1;
-            while(!q.empty()){
-                
-                    pair<int,int>temp=q.front();
-                    q.pop();
-                    
-                    for(int i=0;i<directions.size();i++){
-        
-                            int x=temp.first+directions[i][0];
-                            int y=temp.second+directions[i][1];
-                        
-                            if(temp.first==grid.size()-1 and temp.second==grid[0].size()-1){
-                                
-                                        return gzip[{temp.first,temp.second}];
-                                    
-                            }
-                            if(x>=0 and x<grid.size() and y>=0 and y<grid.size() and grid[x][y]==0 and gzip.find({x,y})==gzip.end()){
-                                    
-                                            gzip[{x,y}]=gzip[{temp.first,temp.second}]+1;
-                                            q.push({x,y});
-                                
-                                
-                            }
-                    
-                    }
-                
-                
-                
-            }
-     
-            return -1;
-    }
     
+    
+ 
+    
+    
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        
+        vector<vector<int>>directions{{1,0},{-1,0},{0,1},{0,-1},{-1,1},{1,-1},{-1,-1},{1,1}};
+        
+       
+        
+        int m=grid.size()-1;
+        int n=grid[0].size()-1;
+        if(grid[0][0] or grid[m][n]){
+                return -1;
+        }        
+        
+         vector<vector<bool>>seen(m+1,vector<bool>(n+1,false));
+        queue<vector<int>>q;
+        q.push({0,0,1});
+        
+        while(!q.empty()){
+            
+            vector<int>ans=q.front();
+            q.pop();
+            if(ans[0]==m and ans[1]==n)
+                    return ans[2];
+                    
+            for(int i=0;i<directions.size();i++){
+                
+                int x=ans[0]+directions[i][0];
+                int y=ans[1]+directions[i][1];
+                
+                if(x>=0 and x<=m and y>=0 and y<=n and seen[x][y]==false and grid[x][y]!=1){
+                    
+                            // cout<<x<<"\t"<<y<<ans[2]+1<<endl;
+                            q.push({x,y,ans[2]+1});
+                            seen[x][y]=true;
+                    
+                }
+                
+                
+            }    
+            
+            
+            
+        }
+        
+        return -1;
+        
+        
+    }
 };
