732d7dbb3b1c0dd050b844c85b332f90ec4a98f6
Mohd Zaid <diaz.zidane@gmail.com>
2023-09-04 11:38:56 -0600

    Time: 1046 ms (5.03%), Space: 247.4 MB (5.03%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     
     
-    void dfs(vector<vector<int>>&grid,int i,int j){
+    void dfs(vector<vector<int>>&grid,int i,int j,vector<vector<bool>>&visited){
         
             vector<vector<int>>directions{{1,0},{-1,0},{0,1},{0,-1}};
-            if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j]==0)
+            if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j]==0 or visited[i][j]==true)
                     return;
-            
+        
+            visited[i][j]=true;
             grid[i][j]=0;
             for(int k=0;k<directions.size();k++){
                     int x =i+directions[k][0];
                     int y =j+directions[k][1];
-                    dfs(grid,x,y);
+                    dfs(grid,x,y,visited);
             }
     }
     
@@ -21,23 +22,23 @@ public:
         
                 int m=grid.size();
                 int n=grid[0].size();
-                //vector<vector<bool>>visited(m,vector<bool>(n,false));
+                vector<vector<bool>>visited(m,vector<bool>(n,false));
             
                 for(int j=0;j<grid[0].size();j++){
                             if(grid[0][j]==1){   
-                                    dfs(grid,0,j);
+                                    dfs(grid,0,j,visited);
                             }
                             if(grid[m-1][j]==1){                                
-                                    dfs(grid,m-1,j);
+                                    dfs(grid,m-1,j,visited);
                             }
                 }
 
                 for(int i=0;i<grid.size();i++){
                                 if(grid[i][0]==1){   
-                                        dfs(grid,i,0);
+                                        dfs(grid,i,0,visited);
                                 }
                                 if(grid[i][n-1]==1){                                
-                                        dfs(grid,i,n-1);
+                                        dfs(grid,i,n-1,visited);
                                 }
                 }
             
