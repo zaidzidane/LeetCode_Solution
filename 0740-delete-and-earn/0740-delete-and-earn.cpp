5131bf0ea6cf5db2d16479b94b19a869195d55c8
Mohd Zaid <diaz.zidane@gmail.com>
2023-09-11 21:10:00 -0600

    Time: 0 ms (100.00%), Space: 13.9 MB (44.10%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     
+    
+    
     int deleteAndEarn(vector<int>& nums) {
     
-        unordered_map<int,int>gzip;
-        int max_number=-1;
-        for(int i=0;i<nums.size();i++){
+            int n=10001;
+            vector<int>v(n,0);
+           
+            for(auto item:nums){
+                    v[item]+=item;
+            }
             
-                   gzip[nums[i]]+=nums[i]; 
-                   max_number=max(max_number,nums[i]);
-        }
+            int take=0;
+            int skip=0;
+            for(int i=0;i<n;i++){
+                int takei=skip+v[i];
+                int skipi=max(skip,take);
+                take=takei;
+                skip=skipi;
+                
+            }
         
-        vector<int>v(max_number+3);
-        
-        for(int i=2;i<v.size();i++){
-            
-                v[i]=max(v[i-1],v[i-2]+gzip[i-2]);
-            
-        }
-        
-        int max_=0;
-        for(int i=0;i<v.size();i++){
-                        
-                    cout<<i<<"\t"<<v[i]<<endl;
-                    max_=max(max_,v[i]);
-            
-        }
-        
-        return max_;
+            return max(take,skip);
     }
 };
