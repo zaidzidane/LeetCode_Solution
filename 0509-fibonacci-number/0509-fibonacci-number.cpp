59276567ba39880bba229a1a172618127c7b0f2b
Mohd Zaid <diaz.zidane@gmail.com>
2023-09-11 18:23:14 -0600

    Time: 0 ms (100.00%), Space: 5.9 MB (61.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
+            
+            if(n<=1){
+                    return n;
+            }
+
+            int ans=0;
+            int a=0;
+            int b=1;
+            for(int i=2;i<=n;i++){
+                    ans=a+b;
+                    a=b;
+                    b=ans;
+            }
         
-            if(n==0)
-                    return 0;
-                
-            if(n==1)
-                    return 1;
-                
-            return fib(n-1)+fib(n-2);
-                
-                
+            return ans;
+        
     }
 };
