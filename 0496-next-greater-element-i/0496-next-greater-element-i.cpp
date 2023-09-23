be100cf613e04864d83c80fc127fc9f27835b343
Mohd Zaid <diaz.zidane@gmail.com>
2023-09-23 08:43:40 -0600

    Time: 3 ms (87.87%), Space: 9.5 MB (6.79%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,48 +2,39 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-            stack<int>s;
-            unordered_map<int,int>gzip;
-        
-            for(int i=0;i<nums2.size();i++){
+        stack<int>s;
+        unordered_map<int,int>gzip;
+        for(int i=0;i<nums2.size();i++){
                 
-                            if(s.empty()){
-                                
-                                    s.push(nums2[i]);
-                            }
+                    if(s.empty()){
+                            s.push(i);
+                    }    
+                    while(!s.empty() and nums2[s.top()]<nums2[i]){
+                        
                             
-                            if(s.top()<nums2[i]){
-                                    
-                                        while(!s.empty() and s.top()<nums2[i] ){
-                                            
-                                                gzip[s.top()]=nums2[i];
-                                                s.pop();
-                                        
-                                        }       
-                            }
-                            
-                                
-                                        s.push(nums2[i]);
-                                    
-                            
-            }
+                            gzip[nums2[s.top()]]=nums2[i];
+                            s.pop();
+                    }
             
-            while(!s.empty()){
-                
-                    gzip[s.top()]=-1;
-                    s.pop();
-    
-            }
+                s.push(i);
+        }
             
-            vector<int>ans;
-            for(int i=0;i<nums1.size();i++){
-                
-                            ans.push_back(gzip[nums1[i]]);
-                
-            }
-    
+       
+        vector<int>ans;
+        for(auto item:nums1){
+                if(gzip.find(item)!=gzip.end()){
+                    
+                            ans.push_back(gzip[item]);
+                    
+                }
+            
+                else{
+                        ans.push_back(-1);
+                }
+            
+        }
         
-            return ans;
+        return ans;
         
     }
 };
