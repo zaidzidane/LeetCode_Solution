4d396d7996ac908b4deb27c3c22e61cf7bcfc20e
Mohd Zaid <diaz.zidane@gmail.com>
2024-02-03 11:12:04 -0700

    Time: 186 ms (73.91%), Space: 17.2 MB (86.59%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,45 +1,29 @@
 class Solution:
-    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
-        numNodes=len(isConnected)
-        rank=[1]*numNodes
-        parent=[x for x in range(numNodes)]
         
-        def find(x,parent)->int:
+        
+            def dfs(isConnected,i,visited):
+                
+                for j,node in enumerate(isConnected[i]):
+                    if node==1 and visited[j]==False and i!=j:
+                            visited[j]=True
+                            dfs(isConnected,j,visited)
+                            
+               
             
-            while(x==parent[x]):
-                    return x
+            visited=[False]*len(isConnected)
+            cnt=0
+            for i in range(len(isConnected)):
                 
-            parent[x]=find(parent[x],parent)
-            return parent[x]
-        
-        
-        def union(x,y,parent,rank):
-            parent_x=find(x,parent)
-            parent_y=find(y,parent)
-            if rank[parent_x]>rank[parent_y]:
-                    parent[parent_y]=parent_x
-                    rank[parent_x]+=rank[parent_y]
-                    
-            else:
-                    parent[parent_x]=parent_y
-                    rank[parent_y]+=rank[parent_x]
-                    
-        
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[i])):
-                    if isConnected[i][j]==1 and i<j:
-                        union(i,j,parent,rank) 
-                        
-                
-
-        s=set()
-        for p in parent:
-            s.add(find(p,parent))
-                
-
-        return len(s)
-                    
+                    if not visited[i]:
+                            visited[i]=True
+                            dfs(isConnected,i,visited)
+                            cnt+=1
+                            
             
-                
+            return cnt    
+            
+        
+            
+        
