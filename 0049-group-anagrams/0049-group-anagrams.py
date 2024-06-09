e90b9dd37271b8785f26632a4109f712e67cedff
Mohd Zaid <diaz.zidane@gmail.com>
2024-06-08 19:00:59 -0600

    Time: 101 ms (18.79%), Space: 21.4 MB (30.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,30 @@
 class Solution:
+    
+    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    
         
-        dicti={}
-        for word in strs:
+            dicti={}
+            for item in strs:
+                
+                word_dic=[0]*27
+                for i in item:
+                
+                    word_dic[ord(i)-97]+=1
+                
+                if tuple(word_dic) not in dicti:
+                    dicti[tuple(word_dic)]=[]
+                
+                dicti[tuple(word_dic)].append(item)
+                
+                
             
-            w=''.join(sorted(word))
-            if w not in dicti:
-                dicti[w]=[]
-            
-            dicti[w].append(word)
-        
-        return dicti.values()
-            
-            
-            
-        
+            ans=[]
+            for key,value in dicti.items():
+                
+                ans.append(value)
+                
+                
+            return ans        
+                    
+                    
