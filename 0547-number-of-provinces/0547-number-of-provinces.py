7715316266bdd74bdde3151cc9fda439dfdc9678
Mohd Zaid <diaz.zidane@gmail.com>
2024-06-22 13:40:24 -0600

    Time: 202 ms (13.52%), Space: 17.6 MB (22.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,29 +1,51 @@
 class Solution:
+    
+    def find(self,parent,i):
+        
+        if parent[i]==i:
+            return i
+        
+        parent[i]=self.find(parent,parent[i])
+        return parent[i]
+    
+    def unionset(self,rank,parent,i,j):
+        
+        parent_i=self.find(parent,i)
+        parent_j=self.find(parent,j)
+        if parent_i>parent_j:   
+                rank[parent_i]+= rank[parent_j]
+                parent[parent_j]=parent[parent_i]
+        
+        else:
+                rank[parent_j]+= rank[parent_i]
+                parent[parent_j]=parent[parent_i]
+        
+        
+        
+    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    
         
-        
-        
-            def dfs(isConnected,i,visited):
-                
-                for j,node in enumerate(isConnected[i]):
-                    if node==1 and visited[j]==False and i!=j:
-                            visited[j]=True
-                            dfs(isConnected,j,visited)
+        rank=[-1 for i in range(len(isConnected))]
+        parent=[i for i in range(len(isConnected))]
                             
-               
-            
-            visited=[False]*len(isConnected)
-            cnt=0
-            for i in range(len(isConnected)):
-                
-                    if not visited[i]:
-                            visited[i]=True
-                            dfs(isConnected,i,visited)
-                            cnt+=1
                             
-            
-            return cnt    
-            
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
+                    
+                    if isConnected[i][j]==1 and i!=j:
+                            self.unionset(rank,parent,i,j)
+                            print(i,j)
+        
+           
+    
+        visited=[]
+        for i in range(len(parent)):
+                parent[i]=self.find(parent,i)
+                
+        return len(set(parent))
+        
         
             
         
+            
