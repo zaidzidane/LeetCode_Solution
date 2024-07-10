26b4e431dbcb7dbb20ff64735342999789338c14
Mohd Zaid <diaz.zidane@gmail.com>
2024-07-09 21:27:19 -0600

    Time: 853 ms (11.34%), Space: 57 MB (72.85%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+    
+            ans=0
+            start=customers[0][0]
+            for customer in customers:
+                 
+                    if start<customer[0]: 
+                            ans+=customer[1]
+                            start=customer[0]+customer[1]
+                    else:
+                        ans+=customer[1]+start-customer[0]
+                        start+=customer[1]
+                    
+                    print(ans)
+            return ans/len(customers)
