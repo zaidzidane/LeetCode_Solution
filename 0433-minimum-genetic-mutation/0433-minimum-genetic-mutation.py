474367d5d1df6fb715fed0d1a7057592cd903063
Mohd Zaid <diaz.zidane@gmail.com>
2024-07-20 13:18:18 -0600

    Time: 30 ms (85.80%), Space: 16.6 MB (30.79%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    
+    
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+    
+    
+            queue=[[startGene,0]]
+            seen={startGene}
+            
+            while(len(queue)):
+               
+                temp,dist=queue.pop(0)
+                print(temp)
+                for char in "ACGT":
+                    for i in range(len(temp)):
+                        s = temp[:i]+char+temp[i+1:]
+                        if s  in bank and s not in seen:
+                            if s==endGene:
+                                    return dist+1
+                            queue.append([s,dist+1])
+                            seen.add(s)
+            
+                
+            
+                      
+            return -1
